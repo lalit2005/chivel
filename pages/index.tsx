2818a1f7e602f6feacd3ec5db303d594bc0238d7
Lalit <lalitvijay9480@gmail.com>
2021-12-06 20:49:46 +0530

    feat: add navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,9 @@ import Link from "next/link";
 const Home: NextPage = () => {
   return (
     <div className="min-h-screen text-white bg-black">
-      {/* <nav className="max-w-2xl py-4 mx-auto text-gray-300 hover:text-gray-50">
-        <ul className="flex items-center justify-between">
+      <nav className="flex items-center justify-between max-w-4xl px-5 py-4 mx-auto text-gray-300 hover:text-gray-50 sm:px-0">
+        <p className="text-2xl font-bold text-white font-cal">CHIVEL</p>
+        <ul className="flex items-center justify-between space-x-10">
           <li>
             <Link href="/">Home</Link>
           </li>
@@ -16,7 +17,7 @@ const Home: NextPage = () => {
             <Link href="/">Support</Link>
           </li>
         </ul>
-      </nav> */}
+      </nav>
       <div className="pt-32 text-6xl font-black text-center font-cal">
         <h1>Make landing pages for your mobile apps</h1>
         <span className="text-transparent opacity-80 bg-clip-text bg-gradient-to-r from-yellow-300 via-purple-300 to-blue-500">
