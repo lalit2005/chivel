f5d1de08c5bb6e24503da599125df46765846887
Lalit <lalitvijay9480@gmail.com>
2021-12-06 19:03:13 +0530

    feat: add tailwind, mantine, and prisma

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from "next/app";
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <Component {...pageProps} />
+      {/* </MantineProvider> */}
+    </>
+  );
 }
-
-export default MyApp
