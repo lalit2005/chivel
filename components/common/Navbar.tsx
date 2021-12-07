42a2128386cb7238341f3baf1baed56968f32000
Lalit <lalitvijay9480@gmail.com>
2021-12-07 23:04:06 +0530

    feat: add login page

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -1,26 +1,39 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className='flex items-center justify-between max-w-4xl px-5 py-4 mx-auto text-gray-300 sm:px-0'>
-      <p className='text-2xl font-bold text-white font-cal'>CHIVEL</p>
-      <ul className='flex items-center justify-between space-x-10'>
+    <nav className="flex items-center justify-between max-w-4xl px-5 py-4 mx-auto text-gray-300 sm:px-0">
+      <Link href="/">
+        <a>
+          <p className="text-2xl font-bold text-white select-none font-cal">
+            CHIVEL
+          </p>
+        </a>
+      </Link>
+      <ul className="flex items-center justify-between space-x-10">
         <li>
-          <Link href='/'>Home</Link>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
         </li>
         <li>
-          <Link href='/'>Showcase</Link>
+          <Link href="/">
+            <a>Showcase</a>
+          </Link>
         </li>
         <li>
-          <Link href='/'>Support</Link>
+          <Link href="/">
+            <a>Support</a>
+          </Link>
         </li>
-
         <li>
-          <Link href='/'>Login</Link>
+          <Link href="/auth/login">
+            <a>Login</a>
+          </Link>
         </li>
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,10 @@
+import AppLayout from "@/layouts/AppLayout";
 import type { NextPage } from "next";
 import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
-    <div className="min-h-screen text-white bg-black">
-      <nav className="flex items-center justify-between max-w-4xl px-5 py-4 mx-auto text-gray-300 hover:text-gray-50 sm:px-0">
-        <p className="text-2xl font-bold text-white select-none font-cal">
-          CHIVEL
-        </p>
-        <ul className="flex items-center justify-between space-x-10">
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/">Showcase</Link>
-          </li>
-          <li>
-            <Link href="/">Support</Link>
-          </li>
-        </ul>
-      </nav>
+    <AppLayout>
       <div className="pt-32 text-6xl font-black text-center font-cal">
         <h1>
           Create a stunning landing page <br /> for your Youtube channel
@@ -38,7 +23,7 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
-    </div>
+    </AppLayout>
   );
 };
 
