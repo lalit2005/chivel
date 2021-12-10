5c6c1a79629eac95bdedf07f444b282cf4a7ef2b
Lalit <lalitvijay9480@gmail.com>
2021-12-10 22:13:26 +0530

    fix: fallback option of generated pages

--- pages/_sites/[channel].tsx
+++ pages/_sites/[channel].tsx
@@ -85,6 +85,6 @@ export const getStaticProps: GetStaticProps = ({ params }) => {
 export const getStaticPaths: GetStaticPaths = () => {
   return {
     paths: [],
-    fallback: true,
+    fallback: 'blocking',
   };
 };
