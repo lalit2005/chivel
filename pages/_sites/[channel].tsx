d57f09a83bf2735791ed02ce02281050f02cf000
Ashik Chapagain <projectashik@gmail.com>
2021-12-11 18:13:02 +0545

    Editted Channel Page

--- pages/_sites/[channel].tsx
+++ pages/_sites/[channel].tsx
@@ -1,9 +1,9 @@
 import axios from 'axios';
+import { url } from 'inspector';
 import { GetStaticPaths, GetStaticProps } from 'next';
 
 // @ts-ignore
 const Page = ({ data }) => {
-  console.log(data);
   const navLinks = [
     {
       text: 'Apple',
@@ -16,44 +16,53 @@ const Page = ({ data }) => {
   ];
 
   return (
-    <div className='w-screen min-h-screen text-white bg-black'>
-      <div className='flex justify-between w-full max-w-4xl py-6 mx-auto sm:px-0'>
+    <div className='text-white bg-black'>
+      <div className='flex flex-col min-h-screen justify-between w-full max-w-4xl py-6 mx-auto sm:px-0'>
         {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src={data.avatar} width='50' height='50' alt={data.title} />
         <nav className='flex items-center justify-between space-x-6'>
-          {navLinks.map((navLink) => {
-            return (
-              <a
-                href={navLink.href}
-                key={navLink.text}
-                className='opacity-75 hover:opacity-100'>
-                {navLink.text}
-              </a>
-            );
-          })}
+          <img src={data.avatar} width='50' height='50' alt={data.title} />
+          <ul className='flex gap-4'>
+            {navLinks.map((navLink) => {
+              return (
+                <li>
+                  <a
+                    href={navLink.href}
+                    key={navLink.text}
+                    className='opacity-75 hover:opacity-100'>
+                    {navLink.text}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
         </nav>
+        <main className='mt-32'>
+          <div className='text-center'>
+            <div className='relative'>
+              <div className='absolute max-w-xl mx-auto -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 rounded-lg opacity-50 transition filter blur-2xl duration-1000 animate-tilt'></div>
+              <h1 className='font-extrabold capitalize text-7xl'>
+                {data.title}
+              </h1>
+            </div>
+            <p className='mt-8 text-lg text-gray-400 max-w-2xl mx-auto'>
+              {data.description}
+            </p>
+            <div className='my-20'>
+              <a
+                href={`https://youtube.com/channel/${data.id}`}
+                target='_blank'
+                rel='noopener noreferrer'
+                className='inline-block px-12 py-4 text-2xl font-bold text-gray-200 transition-all duration-150 transform bg-gray-900 border border-gray-800 rounded font-cal hover:text-gray-100 hover:scale-105'>
+                Visit channel &rarr;
+              </a>
+            </div>
+          </div>
+        </main>
       </div>
-      <main className='mt-36' style={{ minHeight: 'calc(100vh - 82px)' }}>
-        <div className='text-center'>
-          <div className='relative'>
-            <div className='absolute max-w-xl mx-auto -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 rounded-lg opacity-50 transition filter blur-2xl duration-1000 animate-tilt'></div>
-            <h1 className='font-extrabold capitalize text-7xl'>{data.title}</h1>
-          </div>
-          <p className='mt-8 text-lg text-gray-400 max-w-2xl mx-auto'>
-            {data.description}
-          </p>
-          <div className='mt-32'>
-            <a
-              href={`https://youtube.com/channel/${data.id}`}
-              target='_blank'
-              rel='noopener noreferrer'
-              className='inline-block px-12 py-4 text-2xl font-bold text-gray-200 transition-all duration-150 transform bg-gray-900 border border-gray-800 rounded font-cal hover:text-gray-100 hover:scale-105'>
-              Visit channel &rarr;
-            </a>
-          </div>
-        </div>
-      </main>
-      <section className='py-20 bg-red-500 mt-[-175px] text-2xl'>
+
+      <section
+        className='py-20 bg-red-500 text-2xl bg-no-repeat bg-cover bg-center relative'
+        style={{ backgroundImage: `url(${data.banner})` }}>
         <div className='flex items-center justify-between w-full max-w-4xl mx-auto'>
           <div>
             <p className='text-5xl font-bold font-cal'>21k</p>
@@ -79,7 +88,7 @@ export default Page;
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const res = await axios.get(
-    'http://localhost:3000/api/get-youtube?id=UC_x5XG1OV2P6uZZ5FSM9Ttw'
+    'http://localhost:3000/api/get-youtube?id=UCsBjURrPoezykLs9EqgamOA'
   );
   return {
     props: {
