ba4996f4c18da8f4340336229709d312087c4ae2
Ashik Chapagain <projectashik@gmail.com>
2021-12-11 17:50:35 +0545

    Successfully fetched data from youtube api

--- pages/_sites/[channel].tsx
+++ pages/_sites/[channel].tsx
@@ -1,7 +1,11 @@
+import axios from 'axios';
+import { channel } from 'diagnostics_channel';
 import { GetStaticPaths, GetStaticProps } from 'next';
+import useSWR from 'swr';
 
 // @ts-ignore
-const Page = ({ name }) => {
+const Page = ({ data }) => {
+  console.log(data);
   const navLinks = [
     {
       text: 'Apple',
@@ -17,7 +21,7 @@ const Page = ({ name }) => {
     <div className='w-screen min-h-screen text-white bg-black'>
       <div className='flex justify-between w-full max-w-4xl px-4 py-6 mx-auto sm:px-0'>
         {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src='https://picsum.photos/50/50' alt='Fireship' />
+        <img src={data.avatar} width='50' height='50' alt={data.title} />
         <nav className='flex items-center justify-between space-x-6'>
           {navLinks.map((navLink) => {
             return (
@@ -35,15 +39,14 @@ const Page = ({ name }) => {
         <div className='text-center'>
           <div className='relative'>
             <div className='absolute max-w-xl mx-auto -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 rounded-lg opacity-50 transition filter blur-2xl duration-1000 animate-tilt'></div>
-            <h1 className='font-extrabold capitalize text-7xl'>{name}</h1>
+            <h1 className='font-extrabold capitalize text-7xl'>{data.title}</h1>
           </div>
-          <p className='mt-8 text-lg text-gray-400'>
-            Fireship.io is a gateway drug for developers who want to build
-            awesome web & mobile apps.
+          <p className='mt-8 text-lg text-gray-400 max-w-2xl mx-auto'>
+            {data.description}
           </p>
           <div className='mt-32'>
             <a
-              href='https://youtube.com/fireship'
+              href={`https://youtube.com/channel/${data.id}`}
               target='_blank'
               rel='noopener noreferrer'
               className='inline-block px-12 py-4 text-2xl font-bold text-gray-200 transition-all duration-150 transform bg-gray-900 border border-gray-800 rounded font-cal hover:text-gray-100 hover:scale-105'>
@@ -74,10 +77,13 @@ const Page = ({ name }) => {
 
 export default Page;
 
-export const getStaticProps: GetStaticProps = ({ params }) => {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const res = await axios.get(
+    'http://localhost:3000/api/get-youtube?id=UC_x5XG1OV2P6uZZ5FSM9Ttw'
+  );
   return {
     props: {
-      name: params?.channel,
+      data: res.data.data,
     },
   };
 };
