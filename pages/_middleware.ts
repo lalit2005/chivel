46a2a5119560ad654eaf9002050cccc96cbe51c7
Lalit <lalitvijay9480@gmail.com>
2021-12-11 11:36:09 +0530

    fix: subdomain routing

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -5,10 +5,6 @@ export function middleware(req: NextRequest, ev: NextFetchEvent) {
   const host = req.headers.get('host');
   const { pathname } = req.nextUrl;
 
-  if (host === 'chivel.tk' || 'localhost:3000') {
-    return NextResponse.next();
-  }
-
   if (host?.includes('localhost:3000')) {
     if (host.includes('.')) {
       // return NextResponse.rewrite(`/auth/login`);
@@ -21,12 +17,11 @@ export function middleware(req: NextRequest, ev: NextFetchEvent) {
     return new Response(null, { status: 404 });
   }
 
+  if (host === 'chivel.tk' || 'localhost:3000') {
+    return NextResponse.next();
+  }
+
   const subdomain = host?.split('.')[0];
 
-  if (
-    !pathname.includes('.') && // exclude all files in the public folder
-    !pathname.startsWith('/api') // exclude all API routes
-  ) {
-    return NextResponse.rewrite(`/_sites/${subdomain}`);
-  }
+  return NextResponse.rewrite(`/_sites/${subdomain}`);
 }
