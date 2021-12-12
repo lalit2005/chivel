533f6e65c9c75879f00297bd1904e32ab49ea2e0
Lalit <lalitvijay9480@gmail.com>
2021-12-12 19:05:57 +0530

    refactor: show confirming modal while deleting

--- pages/site/[id]/settings.tsx
+++ pages/site/[id]/settings.tsx
@@ -1,16 +1,34 @@
 import { BsTrash } from 'react-icons/bs';
+import { useState } from 'react';
 import DashboardLayout from '@/layouts/DashboardLayout';
+import Modal from '@/common/Modal';
 
 const Setup = () => {
+  const [deleteModal, setDeleteModal] = useState(true);
+
   return (
     <DashboardLayout
       heading='Settings'
       description="Just visit this if you don't want your site anymore."
       page='settings'>
-      <button className='px-4 bg-red-500 py-2 rounded hover:bg-red-700'>
+      <button
+        className='px-4 bg-red-500 py-2 rounded hover:bg-red-700'
+        onClick={() => setDeleteModal(true)}>
         <BsTrash className='inline-block font-bold relative -top-px' /> Delete
         this site
       </button>
+      <Modal
+        title='Are you sure? This cannot be undone!'
+        isOpen={deleteModal}
+        setIsOpen={setDeleteModal}>
+        <button
+          className='px-4 bg-red-500 py-2 rounded hover:bg-red-700'
+          onClick={() => {
+            // TODO: Delete site
+          }}>
+          Yes, Proceed
+        </button>
+      </Modal>
     </DashboardLayout>
   );
 };
