7499726003ae40279f7d30fe0149fe8719624865
Lalit <lalitvijay9480@gmail.com>
2021-12-13 20:59:01 +0530

    feat: add og image tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,32 @@ import { UserContextProvider } from '@/utils/contexts/useUser';
 import { SWRConfig } from 'swr';
 import { Toaster } from 'react-hot-toast';
 import Script from 'next/script';
+import { NextSeo } from 'next-seo';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className='font-sans'>
+      <NextSeo
+        title='Chivel: Get a landing page for your YouTube channel.'
+        description='Just let Chivel know about a YouTube channel, and we get you a blazing fast and SEO friendly landing page for your amazing YT channel'
+        openGraph={{
+          url: 'https://chivel.tk',
+          title: 'Chivel: Get a landing page for your YouTube channel.',
+          description:
+            'Just let Chivel know about a YouTube channel, and we get you a blazing fast and SEO friendly landing page for your amazing YT channel',
+          images: [
+            {
+              url: 'https://chivel.tk/ogimage.png',
+              height: 600,
+              width: 1200,
+              alt: 'Chivel',
+            },
+          ],
+        }}
+        twitter={{
+          cardType: 'summary_large_image',
+        }}
+      />
       {process.env.NODE_ENV === 'production' && (
         <Script
           strategy='lazyOnload'
