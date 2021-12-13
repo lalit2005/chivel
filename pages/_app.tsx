6ec94dc86811fdf57a39b674eef1c8e7af1c0fcf
Lalit <lalitvijay9480@gmail.com>
2021-12-13 20:11:48 +0530

    feat: add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,18 @@ import '@grikomsn/cal-sans';
 import { UserContextProvider } from '@/utils/contexts/useUser';
 import { SWRConfig } from 'swr';
 import { Toaster } from 'react-hot-toast';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className='font-sans'>
+      {process.env.NODE_ENV === 'production' && (
+        <Script
+          strategy='lazyOnload'
+          data-website-id={process.env.NEXT_PUBLIC_ANALYTICS_ID}
+          src={process.env.NEXT_PUBLIC_ANALYTICS_URL}
+        />
+      )}
       <UserContextProvider>
         <SWRConfig
           value={{ fetcher: (url) => fetch(url).then((res) => res.json()) }}>
